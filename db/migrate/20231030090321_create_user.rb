6a14474e9744fe22c1bebab42f5fc8c302ca9f5c
Jordy van Vorselen <jordy.van.vorselen@kabisa.nl>
2023-10-30 10:04:45 +0100

    Add user model and table

---
+++ db/migrate/20231030090321_create_user.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# User migration
+class CreateUser < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
