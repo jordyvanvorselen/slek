359bfa14bb0e6c655b92d71c524e09ef234ce1bd
Jordy van Vorselen <jordy.van.vorselen@kabisa.nl>
2023-11-06 10:07:55 +0100

    Save the new user, add form validations
    I tend to prefer rescueing the active record exception as it divides
    the happy and unhappy flow in the controller. But if you used something
    like `if @user.save` `else` it's also fine.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,16 @@ class UsersController < ApplicationController
   before_action :user_params, only: %i[create]
 
   def new
+    @user = User.new
   end
 
   def create
-    User.create!(user_params)
+    @user = User.new(user_params)
+    @user.save!
+
+    redirect_to @user, notice: "Welcome #{@user.name}!"
+  rescue ActiveRecord::RecordInvalid
+    render :new, status: :unprocessable_entity
   end
 
   private
