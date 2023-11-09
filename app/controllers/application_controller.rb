760a124edd4c9c7d31565d3f4bf7ac5edcc9cec9
Jordy van Vorselen <jordy.van.vorselen@kabisa.nl>
2023-11-09 13:28:04 +0100

    Check if the user is authenticated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :check_user_authenticated
+
+  private
+
+  def check_user_authenticated
+    return if session[:username]
+
+    redirect_to new_user_path
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@
 
 class UsersController < ApplicationController
   before_action :user_params, only: %i[create]
+  skip_before_action :check_user_authenticated, only: %i[new create]
 
   def new
     @user = User.new
@@ -10,6 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.save!
+
+    session[:username] = @user.name
 
     redirect_to channels_url
   rescue ActiveRecord::RecordInvalid
